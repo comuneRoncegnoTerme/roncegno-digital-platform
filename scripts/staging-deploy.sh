0aaae4c839706937aed149b49884a385818550fa
Comune di Roncegno Terme <lorenzo.bernardi@comune.roncegnoterme.tn.it>
2026-09-18 21:44:02 +0200

    chore(staging): update scripts/staging-deploy.sh

--- scripts/staging-deploy.sh
+++ scripts/staging-deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 set -eu
 BRANCH=develop
+ENV_FILE=${ENV_FILE:-.env.staging}
+
 git fetch origin "$BRANCH"
 git checkout "$BRANCH"
 git pull --ff-only origin "$BRANCH"
-ENV_FILE=${ENV_FILE:-.env.staging} ./scripts/staging-up.sh
-ENV_FILE=${ENV_FILE:-.env.staging} ./scripts/staging-healthcheck.sh
+
+ENV_FILE="$ENV_FILE" sh ./scripts/staging-up.sh
+ENV_FILE="$ENV_FILE" sh ./scripts/staging-healthcheck.sh
 printf 'Deploy staging completato da branch %s.\n' "$BRANCH"
